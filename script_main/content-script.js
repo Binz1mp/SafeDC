f7de81321b3a50c9416c46bbe83230a095951e45
Binz1mp <96367152+Binz1mp@users.noreply.github.com>
2022-05-16 20:52:50 +0900

    SafeDC로 닉변 및 용도 변환

--- script_main/content-script.js
+++ script_main/content-script.js
@@ -1,10 +1,22 @@
-let dcthreads = document.querySelector(".gall_list");
-// setTimeout(() => {
-//   // window.onload = function () {
-    if ( window.location.href.indexOf("lists/?id=") > -1 && dcthreads.className.indexOf("empty") > -1) {
-      location.reload();
-    } else if ( window.location.href.indexOf("gall.dcinside.com/mgallery/board/lists?id=") > -1 && dcthreads.className.indexOf("empty") > -1) {
-      location.reload();
-    }
-//   // };
-// }, 1000);
+$(document).ready(function () {
+let dcpost = $('.write_div');
+let gallviewcontents = $('.gallview_contents');
+let allImgElement = $('img');
+let allVidElement = $('video');
+let dccons = $('.written_dccon');
+let sizerbutton = $('<button id = "sizer">이미지 모두 보이기</button>');
+dcpost.find(allImgElement).not(dccons).hide();
+dcpost.find(allVidElement).not(dccons).hide();
+dcpost.after(sizerbutton);
+gallviewcontents.prepend(sizerbutton);
+let dcimageStatusImage = dcpost.find(allImgElement).not(dccons);
+let dcimageStatusVideo = dcpost.find(allVidElement).not(dccons);
+
+sizerbutton.each(function() {
+  $(this).on("click", function () {
+    dcimageStatusImage.toggle("show");
+    dcimageStatusVideo.toggle("show");
+    });
+  }); 
+});
+
