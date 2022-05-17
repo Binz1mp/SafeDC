92d65403fe6f82615a42bb375e597e4e0f58c09f
Binz1mp <96367152+Binz1mp@users.noreply.github.com>
2022-05-17 11:42:43 +0900

    마갤대응패치

--- script_main/content-script.js
+++ script_main/content-script.js
@@ -1,21 +1,22 @@
 $(document).ready(function () {
-let dcpost = $('.write_div');
-let gallviewcontents = $('.gallview_contents');
-let allImgElement = $('img');
-let allVidElement = $('video');
-let dccons = $('.written_dccon');
-let sizerbutton = $('<button id = "sizer">SafeDC</button>');
-dcpost.find(allImgElement).not(dccons).hide();
-dcpost.find(allVidElement).not(dccons).hide();
-gallviewcontents.prepend(sizerbutton);
-let dcimageStatusImage = dcpost.find(allImgElement).not(dccons);
-let dcimageStatusVideo = dcpost.find(allVidElement).not(dccons);
+  if (window.location.href.indexOf("board/view") > -1) {
+    let dcpost = $('.write_div');
+    let gallviewcontents = $('.gallview_contents');
+    let allImgElement = $('img');
+    let allVidElement = $('video');
+    let dccons = $('.written_dccon');
+    let sizerbutton = $('<button id = "sizer">SafeDC</button>');
+      dcpost.find(allImgElement).not(dccons).hide();
+      dcpost.find(allVidElement).not(dccons).hide();
+      gallviewcontents.prepend(sizerbutton);
+    let dcimageStatusImage = dcpost.find(allImgElement).not(dccons);
+    let dcimageStatusVideo = dcpost.find(allVidElement).not(dccons);
 
-sizerbutton.each(function() {
-  $(this).on("click", function () {
-    dcimageStatusImage.toggle("show");
-    dcimageStatusVideo.toggle("show");
-    });
-  }); 
+    sizerbutton.each(function() {
+      $(this).on("click", function () {
+        dcimageStatusImage.toggle("show");
+        dcimageStatusVideo.toggle("show");
+        });
+    }); 
+  }
 });
-
