17fe46e3897c72c0666defecc79ee56f67c70cd5
Binz1mp <96367152+Binz1mp@users.noreply.github.com>
2022-05-17 13:55:45 +0900

    온오프 기능 추가 및 썸네일로 변경

---
+++ options.js
@@ -0,0 +1,9 @@
+const safeDCimg = document.getElementById('safeDCimg');
+////////////
+chrome.storage.sync.get('isSafeDCOn', (storage) => {
+  safeDCimg.checked = storage.isSafeDCOn;
+});
+safeDCimg.addEventListener("click", (event) => {
+  chrome.storage.sync.set({"isSafeDCOn": safeDCimg.checked});
+})
+////////////
--- script_main/content-script.js
+++ script_main/content-script.js
@@ -1,22 +1,22 @@
-$(document).ready(function () {
-  if (window.location.href.indexOf("board/view") > -1) {
-    let dcpost = $('.write_div');
-    let gallviewcontents = $('.gallview_contents');
-    let allImgElement = $('img');
-    let allVidElement = $('video');
-    let dccons = $('.written_dccon');
-    let sizerbutton = $('<button id = "sizer">SafeDC</button>');
-      dcpost.find(allImgElement).not(dccons).hide();
-      dcpost.find(allVidElement).not(dccons).hide();
-      gallviewcontents.prepend(sizerbutton);
-    let dcimageStatusImage = dcpost.find(allImgElement).not(dccons);
-    let dcimageStatusVideo = dcpost.find(allVidElement).not(dccons);
+chrome.storage.sync.get('isSafeDCOn', storage => {
+  if (storage.isSafeDCOn) {
+    $(document).ready(function () {
+      if (window.location.href.indexOf("board/view") > -1) {
+        let dcpost = $('.write_div');
+        let gallviewcontents = $('.gallview_contents');
+        let allImgElement = $('img');
+        let allVidElement = $('video');
+        let dccons = $('.written_dccon');
+        let dcimageStatusImage = dcpost.find(allImgElement).not(dccons);
+        let dcimageStatusVideo = dcpost.find(allVidElement).not(dccons);
+        let sizerbutton = $('<button id = "sizer">SafeDC</button>');
+        gallviewcontents.prepend(sizerbutton);
 
-    sizerbutton.each(function() {
-      $(this).on("click", function () {
-        dcimageStatusImage.toggle("show");
-        dcimageStatusVideo.toggle("show");
+        sizerbutton.on("click", function () {
+          dcimageStatusImage.toggleClass('imgThumbnail')
+          dcimageStatusVideo.toggleClass('imgThumbnail')
         });
-    }); 
+      }
+    });
   }
 });
