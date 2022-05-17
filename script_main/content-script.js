9724658525fb981841cb35d4ed2c17d4e650e552
Binz1mp <96367152+Binz1mp@users.noreply.github.com>
2022-05-17 14:03:22 +0900

    썸네일만 보이게

--- script_main/content-script.js
+++ script_main/content-script.js
@@ -4,17 +4,13 @@ chrome.storage.sync.get('isSafeDCOn', storage => {
       if (window.location.href.indexOf("board/view") > -1) {
         let dcpost = $('.write_div');
         let gallviewcontents = $('.gallview_contents');
-        let allImgElement = $('img');
-        let allVidElement = $('video');
         let dccons = $('.written_dccon');
-        let dcimageStatusImage = dcpost.find(allImgElement).not(dccons);
-        let dcimageStatusVideo = dcpost.find(allVidElement).not(dccons);
         let sizerbutton = $('<button id = "sizer">SafeDC</button>');
-        gallviewcontents.prepend(sizerbutton);
 
+        gallviewcontents.prepend(sizerbutton);
         sizerbutton.on("click", function () {
-          dcimageStatusImage.toggleClass('imgThumbnail')
-          dcimageStatusVideo.toggleClass('imgThumbnail')
+          dcpost.find($('img')).not(dccons).toggleClass('imgThumbnail')
+          dcpost.find($('video')).not(dccons).toggleClass('imgThumbnail')
         });
       }
     });
