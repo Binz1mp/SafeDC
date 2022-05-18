0d02bffa821c7aa36a0970e12e5cee7257ebf7e7
Binz1mp <96367152+Binz1mp@users.noreply.github.com>
2022-05-19 00:09:09 +0900

    이미지 있을때만 버튼추가

--- script_main/content-script.js
+++ script_main/content-script.js
@@ -1,21 +1,20 @@
 $(document).ready(function () {
-let dcpost = $('.write_div');
-let gallviewcontents = $('.gallview_contents');
-let allImgElement = $('img');
-let allVidElement = $('video');
-let dccons = $('.written_dccon');
-let sizerbutton = $('<button id = "sizer">SafeDC</button>');
-dcpost.find(allImgElement).not(dccons).hide();
-dcpost.find(allVidElement).not(dccons).hide();
-gallviewcontents.prepend(sizerbutton);
-let dcimageStatusImage = dcpost.find(allImgElement).not(dccons);
-let dcimageStatusVideo = dcpost.find(allVidElement).not(dccons);
-
-sizerbutton.each(function() {
-  $(this).on("click", function () {
-    dcimageStatusImage.toggle("show");
-    dcimageStatusVideo.toggle("show");
-    });
-  }); 
+  let dcpost = $('.write_div');
+  let gallviewcontents = $('.gallview_contents');
+  let dccons = $('.written_dccon');
+  let sizerbutton = $('<button id = "sizer">SafeDC</button>');
+  dcpost.find($('img')).not(dccons).hide();
+  dcpost.find($('video')).not(dccons).hide();
+  let dcimageStatusImage = dcpost.find($('img')).not(dccons);
+  let dcimageStatusVideo = dcpost.find($('video')).not(dccons);
+  if (dcimageStatusImage.length > 0 || dcimageStatusVideo.length > 0) {
+  gallviewcontents.prepend(sizerbutton);
+  sizerbutton.each(function() {
+    $(this).on("click", function () {
+      dcimageStatusImage.toggle("show");
+      dcimageStatusVideo.toggle("show");
+      });
+    }); 
+  }
 });
 
