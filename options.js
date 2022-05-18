9e53aa18e68de5e4772c08eb35554fcce011bbb1
Binz1mp <96367152+Binz1mp@users.noreply.github.com>
2022-05-18 22:46:27 +0900

    나중을 위한 주석 위치 이동

--- options.js
+++ options.js
@@ -1,5 +1,5 @@
-const safeDCimg = document.getElementById('safeDCimg');
 ////////////
+const safeDCimg = document.getElementById('safeDCimg');
 chrome.storage.sync.get('isSafeDCOn', (storage) => {
   safeDCimg.checked = storage.isSafeDCOn;
 });
