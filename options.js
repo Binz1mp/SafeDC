ea41e46e2898030ceb8f5ecf39d48fce4c50a61e
Binz1mp <96367152+Binz1mp@users.noreply.github.com>
2022-05-28 15:14:13 +0900

    이미지 색반전 고침

--- options.js
+++ options.js
@@ -7,3 +7,12 @@ safeDCimg.addEventListener("click", (event) => {
   chrome.storage.sync.set({"isSafeDCOn": safeDCimg.checked});
 })
 //////////// 세이프디씨 기능 온오프 끝 ////////////
+//////////// 야간모드일 경우 이미지 색반전 체크 시작 ////////////
+const safeDCdarkmode = document.getElementById('safeDCdarkmode');
+chrome.storage.sync.get('isSafeDCdark', (storage) => {
+  safeDCdarkmode.checked = storage.isSafeDCdark;
+});
+safeDCdarkmode.addEventListener("click", (event) => {
+  chrome.storage.sync.set({"isSafeDCdark": safeDCdarkmode.checked});
+})
+//////////// 야간모드일 경우 이미지 색반전 체크 끝 ////////////
--- script_main/content-script.js
+++ script_main/content-script.js
@@ -1,24 +1,40 @@
 chrome.storage.sync.get('isSafeDCOn', storage => {
   if (storage.isSafeDCOn) {
     $(document).ready(function () {
+
+      let dcpost = $('.write_div');
+      let gallviewcontents = $('.gallview_contents');
+      let dccons = $('.written_dccon');
+      let sizerbutton = $('<button id = "sizer">SafeDC</button>');
+      let images = $('img');
+      let videos = $('video');
+      let dcimageStatusImage = dcpost.find(images).not(dccons);
+      let dcimageStatusVideo = dcpost.find(videos).not(dccons);
+
       if (window.location.href.indexOf("board/view") > -1) {
-        let dcpost = $('.write_div');
-        let gallviewcontents = $('.gallview_contents');
-        let dccons = $('.written_dccon');
-        let sizerbutton = $('<button id = "sizer">SafeDC</button>');
-        let images = $('img');
-        let videos = $('video');
-        let dcimageStatusImage = dcpost.find(images).not(dccons);
-        let dcimageStatusVideo = dcpost.find(videos).not(dccons);
-        dcpost.find(videos).not(dccons).removeAttr("style");
         if (dcimageStatusImage.length > 0 || dcimageStatusVideo.length > 0) {
           gallviewcontents.prepend(sizerbutton);
-          sizerbutton.on("click", function () {
-            dcimageStatusImage.toggleClass('imgThumbnail');
-            dcimageStatusVideo.toggleClass('imgThumbnail');
-          });
+          dcpost.find(videos).not(dccons).removeAttr("style");
         }
+        chrome.storage.sync.get('isSafeDCdark', storage => {
+          if (!storage.isSafeDCdark) {
+            sizerbutton.on("click", function () {
+              dcimageStatusImage.toggleClass('imgThumbnail');
+              dcimageStatusVideo.toggleClass('imgThumbnail');
+            });
+          } else if (storage.isSafeDCdark) {
+            dcimageStatusVideo.addClass('safeDCdark');
+            dcimageStatusImage.addClass('safeDCdark');
+            sizerbutton.on("click", function () {
+              dcimageStatusImage.toggleClass('safeDCdark');
+              dcimageStatusImage.toggleClass('imgThumbnail-safeDCdark');
+              dcimageStatusVideo.toggleClass('safeDCdark');
+              dcimageStatusVideo.toggleClass('imgThumbnail-safeDCdark');
+            });
+          }
+        });
       }
+
     });
   }
 });
