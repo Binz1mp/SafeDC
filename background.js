6f646d411f8021f5ce85df2f3dec5a4133e4e82f
Binz1mp <96367152+Binz1mp@users.noreply.github.com>
2022-05-28 16:12:27 +0900

    업데이트 로그도 추가

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
 chrome.runtime.onInstalled.addListener(function () {
+  chrome.tabs.create({
+    url: chrome.runtime.getURL("changelog/update.html"),
+  });
   chrome.storage.sync.set({ "isSafeDCOn": true });
+  chrome.storage.sync.set({ "isSafeDCdark": false });
 });
