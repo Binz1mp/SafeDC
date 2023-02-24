d7f516edca544463e7d896238d192f8b2dc84b98
Binz1mp_Laptop <you@example.com>
2023-02-25 07:40:06 +0900

    크롬 업데이트 감지가 잘 안되네

--- background.js
+++ background.js
@@ -1,11 +1,19 @@
-chrome.runtime.onInstalled.addListener(function () {
+chrome.runtime.onInstalled.addListener(function (details) {
   const reason = details.reason;
   switch(reason) {
     // case 'install': {  }
-    case 'update':
+    case 'install':
       chrome.tabs.create({
         url: chrome.runtime.getURL("changelog/update.html"),
       });
+      break;
+    case 'update':
+    chrome.tabs.create({
+      url: chrome.runtime.getURL("changelog/update.html"),
+    });
+    break;
+    case 'chrome_update': break;
+    case 'shared_module_update': break;
   }
   chrome.storage.sync.set({ "isSafeDCdark": false });
 });
